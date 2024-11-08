f65484aafb7b176c1321c302c7fc455f7e096384
biaqwe <biancabalas2905@yahoo.com>
2024-11-08 17:15:28 +0200

    Design changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,37 @@
 import '../css/Home.css';
+import { FaFileMedical } from "react-icons/fa6";
 import vroLogo from '../resources/vro.png';
+import { BiPlusMedical } from "react-icons/bi";
+import { FaClinicMedical } from "react-icons/fa";
+import { RiProfileFill } from "react-icons/ri";
+import { FaHandHoldingMedical } from "react-icons/fa";
+
 
 const Home = () => {
     return(
-    <body>
-          <div className="home-container">
-      <header className="header">
-      <img src={vroLogo} alt="vro" className="logo" />
-      </header>
-      <div className="button-container">
-        <button className="hist">Medical History</button>
-        <button className="appt">Appointments</button>
-        <button className="symp">Log Symptoms</button>
-        <button className="medics">Medics</button>
-        <button className="Profile">Profile</button>
-      </div>
-    </div>
-    </body>
-      
-    )
-  };
-  
-  export default Home;
+        <div className="home-container">
+            <header className="header">
+                <img src={vroLogo} alt="vro" className="logo" />
+            </header>
+            <div className="button-container">
+                <button className="nav-button">
+                <FaFileMedical />
+                </button>
+                <button className="nav-button">
+                <FaHandHoldingMedical />
+                </button>
+                <button className="nav-button">
+                    <BiPlusMedical />
+                </button>
+                <button className="nav-button">
+                <FaClinicMedical />
+                </button>
+                <button className="nav-button">
+                <RiProfileFill />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
